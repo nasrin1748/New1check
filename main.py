d8405e28d65803459cd2495a4475bfcbb5ffadc4
nasrin pathan <79505893+nasrin1748@users.noreply.github.com>
2025-03-17 17:11:22 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import asyncio
+import sys
+import os
+
+async def main():
+
+    q = "what is your favorite colour ? "
+
+    if sys.platform in ('emscripten','wasi'):
+
+        if "PYGBAG" not in os.environ:
+            print("you need pygbag runtime for async input on terminal main thread")
+        else:
+            # import platform
+            # platform.window.set_raw_mode(1)
+
+            # if not using raw input, add EOT manually to flush prints
+            q += sys.__eot__
+
+            def goto_xy(x, y):
+                CSI(f"{y};{x}H")
+
+            CSI("2J")
+            goto_xy(1,10)
+            color = await input(q)
+    else:
+        color = input(q)
+
+    print(f" {color=} ")
+
+asyncio.run(main())
